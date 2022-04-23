cebefea18a0f26ffdd52eec849c121a128ea19e3
anderson santos <anderson.santos.silva@ccc.ufcg.edu.br>
2022-04-23 06:22:52 -0300

    removed license information

--- src/main/java/io/github/andersonstv/listener/GuildListener.java
+++ src/main/java/io/github/andersonstv/listener/GuildListener.java
@@ -1,21 +1,3 @@
-/*
- *     NecronomiBot. A Discord Bot for use with RPGs (RolePlaying Games)
- *     Copyright (C) 2020  Anderson dos Santos Silva
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package io.github.andersonstv.listener;
 
 import net.dv8tion.jda.api.entities.MessageChannel;
@@ -30,9 +12,9 @@ public class GuildListener extends ListenerAdapter {
         String sep = System.lineSeparator();
         String introMessage = "*That is not dead which can eternal lie." + sep +
                 "And with strange aeons even death may die.*" + sep +
-                "Hello, I'm NecronomiBot, a bot made to help running Call of Cthulhu (and other RPGs) " +
+                "Hello, I'm DarkBot, a bot made to help running Chronicles of Darkness " +
                 "on Discord. You can check out the source code at: " +
-                "https://github.com/andersonstv/NecronomiBot";
+                "https://github.com/andersonstv/darkbot";
         if (channel != null){
             channel.sendMessage(introMessage).queue();
         }
